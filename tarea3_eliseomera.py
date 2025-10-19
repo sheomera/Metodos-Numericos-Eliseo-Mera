f7e1d6720641ffc737fb1dace08cf477e173dd44
Jorge Noreña <jonorena@yahoo.com>
2025-10-19 17:08:06 +0000

    actualizado por el profesor

--- tarea3_eliseomera.py
+++ tarea3_eliseomera.py
@@ -33,5 +33,20 @@ print(varianza)
 # 1. Compare estos resultados con la distribución de Poisson. 
 # No es como una distribución de Poisson, ya que el valor de la varianza no es igual al valor de la media, y ni siquiera se acerca
 
+# Comentario: En realidad la distribución tiene
+# una forma similar a la de la distribución de 
+# Poisson. La varianza es distinta por el
+# intervalo anómalo con 10 eventos.
+
 # 2. Discuta en cuáles intervalos cree usted que se detectó una señal que no es contaminación.
 # Yo creo que el que no es contaminación es el intervalo de 10 señales, ya que es anómalo y el ruido sigue una distribución bien definida.
+
+# Comentario: Sería bueno ser más cuantitativo.
+# Asumiendo una distribución de Poisson, se
+# puede calcular la probabilidad de que se
+# detecte un intervalo de 10 eventos. Si en 
+# cambio no crees que es Poisson, neceditas 
+# algún criterio cuantitativo. No se pueden
+# sacar estas conclusionrs "a ojo".
+
+# Nota: 5.0 
